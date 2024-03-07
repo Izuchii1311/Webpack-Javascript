965e36b84af743f8c5842c0a5e5428ae33e9cafa
Izuchii1311 <luthfiramadhan.lr55@gmail.com>
2024-03-07 13:29:23 +0700

    First Commit #4

---
+++ 1ModuleSystem/src/index.js
@@ -0,0 +1,15 @@
+// require('./style.css');
+import * as bootstrap from 'bootstrap';
+import css from './style.scss';
+import { run } from './app/app.js';
+import { AlertService } from './app/alert.service.js';
+import { CalculatorService } from './app/calculator.service.js';
+import { JokesService } from './app/jokes.service.js';
+
+const alertService = new AlertService();
+const calculatorService = new CalculatorService();
+const jokesService = new JokesService();
+
+run(alertService, calculatorService, jokesService);
+
+alert('Hello World');
